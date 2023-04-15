1bc42a87504152457cebc9dc1c48d040ddbfe070
Bram Reuling <bram.reuling@outlook.com>
2023-04-15 12:55:30 +0200

    Added: Unreal Engine project

---
+++ ATProject/Source/ATProject/ATProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ATProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ATProject, "ATProject" );
+ 
